46fea653df897eed667295073bb896272fde1fd9
JanDeBruycker <73557400+JanDeBruycker@users.noreply.github.com>
2020-11-26 11:54:22 +0100

    Adding src files

---
+++ src/README.txt
@@ -0,0 +1,11 @@
+TODO {
+    HOME -> --
+    POMP/SHOP ->  ---
+    CONTACT -> ---
+
+    TRANSPILE -> Whole file (.json) -> Look up 'how to'
+}
+
+IDEAS {
+    connect to DB with selling products (SHOP)
+}
---
+++ src/shop_script.js
@@ -0,0 +1,277 @@
+// Show/Hide Shop images
+const toggleShopDrankImg = () => {
+  var shop_drank_img = document.getElementById("shop_drank_img");
+  if (shop_drank_img.style.display === "block") {shop_drank_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "block";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#shop_drank";
+  }
+}
+const toggleShopConfisImg = () => {
+  var shop_confisserie_img = document.getElementById("shop_confisserie_img");
+  if (shop_confisserie_img.style.display === "block") {shop_confisserie_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "block";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#shop_confis";
+  }
+}
+const toggleShopTabakswarenImg = () => {
+  var shop_tabakswaren_img = document.getElementById("shop_tabakswaren_img");
+  if (shop_tabakswaren_img.style.display === "block") {shop_tabakswaren_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "block";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#shop_tabakswaren";
+  }
+}
+const toggleShopToebehorenImg = () => {
+  var shop_toebehoren_img = document.getElementById("shop_toebehoren_img");
+  if (shop_toebehoren_img.style.display === "block") {shop_toebehoren_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "block";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#shop_toebehoren";
+  }
+}
+const toggleShopVoedingImg = () => {
+  var shop_voeding_img = document.getElementById("shop_voeding_img");
+  if (shop_voeding_img.style.display === "block") {shop_voeding_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "block";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#shop_voeding";
+  }
+}
+const toggleShopVersImg = () => {
+  var shop_vers_img = document.getElementById("shop_vers_img");
+  if (shop_vers_img.style.display === "block") {shop_vers_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_vers_img.style.display = "block";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#shop_vers";
+  }
+}
+const toggleShopAllerleiImg = () => {
+  var shop_allerlei_img = document.getElementById("shop_allerlei_img");
+  if (shop_allerlei_img.style.display === "block") {shop_allerlei_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "block";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#shop_allerlei";
+  }
+}
+const toggleGarageHoutImg = () => {
+  var garage_hout_img = document.getElementById("garage_hout_img");
+  if (garage_hout_img.style.display === "block") {garage_hout_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "block";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#garage_hout";
+  }
+}
+const toggleGarageKolenImg = () => {
+  var garage_kolen_img = document.getElementById("garage_kolen_img");
+  if (garage_kolen_img.style.display === "block") {garage_kolen_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "block";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#garage_kolen";
+  }
+}
+const toggleGaragePelletsImg = () => {
+  var garage_pellets_img = document.getElementById("garage_pellets_img");
+  if (garage_pellets_img.style.display === "block") {garage_pellets_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "block";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#garage_pellets";
+  }
+}
+const toggleGarageGasflessenImg = () => {
+  var garage_gasflessen_img = document.getElementById("garage_gasflessen_img");
+  if (garage_gasflessen_img.style.display === "block") {garage_gasflessen_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "block";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#garage_gasflessen";
+  }
+}
+const toggleGarageOnderhoudImg = () => {
+  var garage_onderhoud_img = document.getElementById("garage_onderhoud_img");
+  if (garage_onderhoud_img.style.display === "block") {garage_onderhoud_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "block";
+    garage_olie_img.style.display = "none";
+
+    window.location = "./shop.html#garage_onderhoud";
+  }
+}
+const toggleGarageOlieImg = () => {
+  var garage_olie_img = document.getElementById("garage_olie_img");
+  if (garage_olie_img.style.display === "block") {garage_olie_img.style.display = "none";}
+  else {
+    shop_drank_img.style.display = "none";
+    shop_confisserie_img.style.display = "none";
+    shop_tabakswaren_img.style.display = "none";
+    shop_toebehoren_img.style.display = "none";
+    shop_voeding_img.style.display = "none";
+    shop_vers_img.style.display = "none";
+    shop_allerlei_img.style.display = "none";
+    garage_hout_img.style.display = "none";
+    garage_kolen_img.style.display = "none";
+    garage_pellets_img.style.display = "none";
+    garage_gasflessen_img.style.display = "none";
+    garage_onderhoud_img.style.display = "none";
+    garage_olie_img.style.display = "block";
+
+    window.location = "./shop.html#garage_olie";
+  }
+}
+
+// Toggle random section to hide all others, toggle it again to hide itself
+toggleShopDrankImg();
+toggleShopDrankImg();
